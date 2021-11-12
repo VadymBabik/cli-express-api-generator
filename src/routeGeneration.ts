2cce45a74bd9ea5b23d949c9aff97731fc31eb74
vadymbabik <vadym.babik@gmail.com>
2021-11-12 16:21:05 +0200

    update routeGeneration

--- src/routeGeneration.ts
+++ src/routeGeneration.ts
@@ -1,19 +1,21 @@
 import getPathSrc from "./services";
-import path from 'path';
+import path from "path";
 import constants from "./constants";
 import fs from "fs/promises";
 
-export const routeGeneration = async (api:string,entityName:string ) => {
-    const apiSrcPath = await getPathSrc();
-    if(apiSrcPath){
-        const apiPath =path.join(apiSrcPath,constants.api_dir,entityName)
-        await fs.mkdir(api,{recursive:true});
-        await fs.writeFile(`${apiPath}/${api}${entityName}.ts`,`import { Request, Response } from "express";\n
-        export const ${api}${entityName} = async (req: Request, res: Response) => {
+export const routeGeneration = async (api: string, entityName: string) => {
+  const apiSrcPath = await getPathSrc();
+  if (apiSrcPath) {
+    const apiPath = path.join(apiSrcPath, constants.api_dir, entityName);
+    await fs.mkdir(apiPath, { recursive: true });
+    await fs.writeFile(
+      `${apiPath}/${api}${constants.ucFirst(entityName)}.ts`,
+      `import { Request, Response } from "express";\n
+export const ${api}${constants.ucFirst(entityName)} = async (req: Request, res: Response) => {
   res.sendStatus(200);
+};`
+    );
+  }
 };
-        `)
-    }
-}
 
-export default routeGeneration
+export default routeGeneration;
